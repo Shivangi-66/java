fbbd402fe8d181243b66f7a23774acb78df7ccec
Shivangi-66 <shivangibhosle12@gmail.com>
2024-08-28 19:14:08 +0530

    variables

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String[] args)
+    {
+        String name ="Shivangi";
+        String name2="Tejas";
+        int age=22;
+        String friend=name2;
+        System.out.println(name);
+        System.out.println(friend);
+        System.out.println(age);
+        System.out.println(name2);
+
+    }
+}
