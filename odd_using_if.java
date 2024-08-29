b085746876b2684aa2b1f0a1796d692f1b05365c
Shivangi-66 <shivangibhosle12@gmail.com>
2024-08-29 23:44:49 +0530

    odd

---
+++ odd_using_if.java
@@ -0,0 +1,14 @@
+// to check whether number is odd
+class odd1
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=100;i++)
+        {
+            if(i%2!=0) // or can also use if(i%2==1)
+            {
+                System.out.println("odd--"+i);
+            }
+        }
+    }
+}
