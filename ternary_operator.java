5f799f072d493923cc235985c5d149278267c577
Shivangi-66 <shivangibhosle12@gmail.com>
2024-08-29 23:35:30 +0530

    ternary operator

---
+++ ternary_operator.java
@@ -0,0 +1,12 @@
+/* ternary operator - exp1?exp2:exp3 */
+class ternary
+{
+    public static void main(String[] args)
+    {
+        int a,b,greater;
+        a=100;
+        b=200;
+        greater=(a>b)?a:b;
+        System.out.println("The greatest number is-"+greater);
+    }
+}
