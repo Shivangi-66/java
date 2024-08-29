64728aecdccd242aa9b51000c71337a7735fd5c1
Shivangi-66 <shivangibhosle12@gmail.com>
2024-08-29 23:24:51 +0530

    add

---
+++ add.java
@@ -0,0 +1,17 @@
+class add
+{
+    public static void main(String[] args)
+    {
+        int a,b,add,sub,div,mul;
+        a=10;
+        b=12;
+        add=a+b;
+        sub=a-b;
+        div=a/b;
+        mul=a*b;
+        System.out.println("subtraction of a and b"+sub);
+        System.out.println("addition a and b is-"+add);
+        System.out.println("division a and b is-"+div);
+        System.out.println("multiplication a and b is-"+mul);
+    }
+}
