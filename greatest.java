efdf5a193ee5b49df08d6fd354c3600b2c84655d
Shivangi-66 <shivangibhosle12@gmail.com>
2024-08-29 23:44:06 +0530

    greatest

---
+++ greatest.java
@@ -0,0 +1,17 @@
+ class greatest {
+    public static void main(String[] args)
+    {
+        int a,b;
+        a=100;
+        b=500;
+        if (a>b)
+        {
+            System.out.println("a is greater than b");
+        }
+        else
+        {
+            System.out.println("b is greater than a");
+
+        }
+    }
+}
