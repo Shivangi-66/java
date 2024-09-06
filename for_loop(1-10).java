523fc6ded0d27ae0de2283b8aec6d1e255ef9dd2
Shivangi Bhosle <shivangibhosle12@gmail.com>
2024-09-06 22:15:32 +0530

    Add files via upload

---
+++ for_loop(1-10).java
@@ -0,0 +1,12 @@
+// using for loop print numbers from 1 to 100
+class number
+{
+    public static void main(String[] args)
+    {
+        //int i;
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("Number from 1 to 10-"+i);
+        }
+    }
+}
